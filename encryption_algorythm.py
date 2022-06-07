e1cc85c3ff4a044c5ab7d25a2083a0102a005e1e
Jeroen Leuenberger <89943734+Jero075@users.noreply.github.com>
2022-06-07 21:33:37 +0200

    Update encryption_algorythm.py

--- encryption_algorythm.py
+++ encryption_algorythm.py
@@ -1,11 +1,6 @@
 ###---Encryption Algorythm--- by Jeroen Leuenberger###
 ###https://github.com/Jero075/Encryption-Algorythm###
 
-###Characters_for_encryption###
-#char_str = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿ'
-###Hash start to identify encrypted strings
-#hash_start = "$#"
-
 ###Encryption function###
 def encrypt(string, hash_start, hash_length, key1, key2, char_str):
     for i in range(len(string)):
