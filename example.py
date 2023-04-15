cc811c9b0c4e926247c883b8c66f2ac2de1946f4
Jeroen Leuenberger <jereileu@proton.me>
2023-04-15 18:34:12 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 ###Example###
-from encryption_algorythm import encrypt
+from hashing_algorythm import hash
 
 hash_start = "$#"
 char_str = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿ'
@@ -7,6 +7,6 @@ hash_length = 128
 key1 = 7
 key2 = 3
 
-string = input("Encrypt: ")
+string = input("Plaintext: ")
 
-print(encrypt(string, key1, key2, char_str, hash_start, hash_length))
+print(hash(string, key1, key2, char_str, hash_start, hash_length))
